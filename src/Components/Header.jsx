62b0e57cb02d3cebf6277f6c9f6e93598fd72818
PANDURANGZURE <pandurangzure6@gmail.com>
2025-12-26 16:47:38 +0530

    hero page done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,17 +2,22 @@ import React from 'react'
 
 function Header() {
   return (
+    <>
     <div className='flex fixed top-0 justify-between w-screen px-20 py-5 '>
         <div className='font-bold text-3xl '>PZ</div>
         <div>
-            <ul className='flex space-x-3 pt-2'>
+            <ul className='flex space-x-3 pt-2 normal'>
                 <li>Home</li>
                 <li>About</li>
                 <li>Portfolio</li>
                 <li>Contact</li>
+                {/*  */}
             </ul>
+            
         </div>
     </div>
+    
+    </>
   )
 }
 
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,15 +1,52 @@
 import React from 'react'
 import '../index.css'
+import pic from '../assets/pic1.png'
+import pic1 from '../assets/pic2.png'
+import { MdOutlineArrowOutward } from "react-icons/md";
+import { Github, Linkedin,Instagram } from 'lucide-react';
+
 
 function Hero() {
   return (
     <>
-    <div className='h-screen flex flex-col justify-center items-center '>
-   
-    <div className='text-8xl normal'>FRONTEND DEVLOPER </div>
-    <div className=' normal'>FRONTEND DEVLOPER </div>
-    <div className='text-8xl bold'>FRONTEND DEVLOPER </div>
+    <div className='w-screen h-screen flex flex-col justify-center p-5   '>
+    <div className='flex justify-between'>
+      <div className='flex'>
+      <img className='md:h-34 h-20' src={pic} alt="" />
+      <img className='md:h-34 h-20' src={pic1} alt="" />
     </div>
+      <div>
+        <MdOutlineArrowOutward className='md:text-9xl text-7xl' />
+      </div>
+    </div>
+    <div>
+      <p className='normal text-xl mt-2 absolute'> Hello, <span className='normal text-orange-500'>I'm</span></p>
+    <p className='md:text-8xl text-5xl normal md:mt-5 mt-8'>PANDURANG ZURE </p>
+    </div>
+    <div>
+      <p className='normal text-xl  absolute '>I'm<span className='normal text-orange-500'> a</span></p>
+    <p  className='md:text-8xl text-5xl bold md:mt-0 mt-5'>FRONTEND DEVLOPER </p>
+    </div>
+    <p className='normal text-xl md:text-2xl  mt-5 '>Running on <span className=' font-extrabold text-orange-500'>8 GB </span>RAM, fueled by unlimited ambition.</p>
+    <div className="flex text-center space-x-8 m-3">
+              <a href="https://github.com/PANDURANGZURE" target="_blank" rel="noopener noreferrer" className="hover:text-gray-800 transition">
+                <Github size={38} />
+              </a>
+              <a href="https://www.linkedin.com/in/pandurang-santosh-zure-au3112/" target="_blank" rel="noopener noreferrer" className="hover:text-gray-800 transition">
+                <Linkedin size={38} />
+              </a>
+              <a href="https://www.instagram.com/_anonymous_3112_/" target="_blank" rel="noopener noreferrer" className="hover:text-gray-800 transition">
+                <Instagram size={38} />
+              </a>
+            </div>
+    <div className='flex justify-end text-right'>
+      <p className='normal md:text-lg justify-end mt-10 text-right '>
+        My goal is to build websites that are not only functional and efficient <br/> but also engaging and intuitive.
+        I pay close attention to design aesthetics and usability,<br/> ensuring that each project is both visually striking and seamless to navigate.
+      </p>
+    </div>
+    </div>
+    
     </>
   )
 }
