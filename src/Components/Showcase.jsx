dac180bf1ec71b987d869bd41a95a15aeccff683
PANDURANGZURE <pandurangzure6@gmail.com>
2025-12-28 00:02:44 +0530

    ps base done

--- src/Components/Showcase.jsx
+++ src/Components/Showcase.jsx
@@ -17,22 +17,23 @@ function Showcase() {
       force3D: true,
       scrollTrigger: {
         trigger: boxRef.current,
-        start: "top 20%",
+        start: "top 15%",
         end: "bottom 60%",
         scrub: 5,
+        markers: true
       },
     });
   }, []);
 
   return (
-    <div>
+    <div className="overflow-hidden">
         <p
         ref={boxRef}
-        className="text-3xl md:text-6xl font-bold bold text-center"
+        className="text-3xl md:text-6xl md:mt-14 z-50 font-bold bold text-center"
       >
         Portfolio Showcase
       </p>
-      <p className="text-center "></p>
+      <p className="text-center bold mt-5 md:text-xl ">Explore my journey through <span className="text-orange-500">projects</span> , <span className="text-orange-500">certifications</span> and <span className="text-orange-500">technical expertise</span>. <br/> Each section represents a milestone in my continuous learning path.</p>
     </div>
   )
 }
