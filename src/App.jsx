969171ecbf0dd92c06443bcf6fbf43efc4128148
PANDURANGZURE <pandurangzure6@gmail.com>
2025-12-27 16:49:19 +0530

    testins

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Cursor from "./Components/Cursor";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Hero from "./Components/Hero"
+import About from "./Components/About";
 
 function App() {
   return (
@@ -9,8 +10,9 @@ function App() {
     <Cursor/>
     <Header/>
     <Hero/>
+    <About/>
+    <div className="h-screen"></div>
     <Footer/>
-      
     </>
   );
 }
---
+++ src/Components/About.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Line from './Line'
+
+gsap.registerPlugin(ScrollTrigger);
+
+function About() {
+  const boxRef = useRef(null);
+
+  useEffect(() => {
+    gsap.to(boxRef.current, {
+      x: 80,        // 👉 right (small)
+      y: -60,       // 👆 up (small)
+      ease: "none",
+      scrollTrigger: {
+        trigger: boxRef.current,
+        start: "top 40%",
+        end: "bottom 40%",
+        scrub: true,
+        // markers: true, // enable if needed
+      },
+    });
+  }, []);
+
+  return (
+    <section className=" flex flex-col items-center justify-center">
+      <p
+        ref={boxRef}
+        className="text-3xl md:text-6xl bold font-bold text-center"
+      >
+        About Me
+      </p>
+      <Line text='Who am i'/>
+    </section>
+  );
+}
+
+export default About;
