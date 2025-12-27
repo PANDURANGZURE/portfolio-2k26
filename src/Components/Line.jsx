45fcaddce319534dc03babb3a5076167b65c2e6d
PANDURANGZURE <pandurangzure6@gmail.com>
2025-12-27 12:46:59 +0530

    font changes in line

--- src/Components/Line.jsx
+++ src/Components/Line.jsx
@@ -4,7 +4,7 @@ export default function InfiniteLine(props) {
   return (
     <div className="w-full overflow-hidden  py-4">
       <motion.div
-        className="flex whitespace-nowrap title text-black text-2xl md:text-4xl  gap-10"
+        className="flex whitespace-nowrap bold text-black text-2xl md:text-4xl  gap-10"
         animate={{ x: ["0%", "-100%"] }}
         transition={{
           repeat: Infinity,
